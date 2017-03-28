cd7396a2d77706c9ee0172b529bcbcdd39f85b6a
Patrick <porchewellness@gmail.com>
2017-03-27 17:15:48 -0700

    finishes chapter 1 exercises

---
+++ command-line/exercises1-6.sh
@@ -0,0 +1,10 @@
+# Write a command to print the string Use "man echo", including the quotes; i.e., take care not to print out Use man echo instead. Hint: Use double quotes in the inner string, and wrap the whole thing in single quotes.
+
+echo 'Use "man echo"'
+
+# By running man sleep, figure out how to make the terminal “sleep” for 5 seconds, and execute the command to do so.
+
+sleep 5
+
+# Use ctrl-c to exit
+sleep 5000
