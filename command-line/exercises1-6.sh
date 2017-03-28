dfd67b20360e1840c3eda71e25b9419c2c79b843
Patrick <porchewellness@gmail.com>
2017-03-27 17:18:41 -0700

    added question to exercise 1-6

--- command-line/exercises1-6.sh
+++ command-line/exercises1-6.sh
@@ -6,5 +6,6 @@ echo 'Use "man echo"'
 
 sleep 5
 
+# Execute the command to sleep for 5000 seconds, realize that’s well over an hour, and then use the instructions from Box 4 to get out of trouble.
 # Use ctrl-c to exit
 sleep 5000
